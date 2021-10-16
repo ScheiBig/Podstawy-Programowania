06ab9f3ce785fdbb834a95a18fc59834a0c5871e
unknown <209337@carbon.cti.p.lodz.pl>
2021-10-16 09:52:06 +0200

    add excercise 1.15

---
+++ 1. Wprowadzanie i wyświetlanie danych/15.Liczby_zespolone.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+#define R 0
+#define I 1
+
+int main()
+{
+    
+    float real1, real2, imag1, imag2;
+
+    printf("Input first numbers real part: ");
+    scanf("%f", &real1);
+    printf("Input first numbers imaginary part: ");
+    scanf("%f", &imag1);
+    printf("Input second numbers real part: ");
+    scanf("%f", &real2);
+    printf("Input first numbers imaginary part: ");
+    scanf("%f", &imag2);
+
+    float sum[] = { real1 + real2, 
+        imag1 + imag2 };
+    float difference[] = { real1 - real2, 
+        imag1 - imag2 };
+    float product[] = { real1 * real2 - imag1 * imag2, 
+        imag1 * real2 + imag2 * real1 };
+    float denominator = real2 * real2 + imag2 * imag2;
+    float quotient[] = { (real1 * real2 + imag1 * imag2) / denominator,
+        (imag1 * real2 - imag2 * real1) / denominator };
+
+    char i1_s[32], i2_s[32], s_s[32], d_s[32], p_s[32], q_s[32];
+    sprintf(i1_s, "%+.3f", imag1);
+    sprintf(i2_s, "%+.3f", imag2);
+    sprintf(s_s, "%+.3f", sum[I]);
+    sprintf(d_s, "%+.3f", difference[I]);
+    sprintf(p_s, "%+.3f", product[I]);
+    sprintf(q_s, "%+.3f", quotient[I]);
+
+    printf("First number: %.3f%ci%s\n", real1, i1_s[0], i1_s+1);
+    printf("Second number: %.3f%ci%s\n", real2, i2_s[0], i2_s+1);
+    printf("Sum: %.3f%ci%s\n", sum[R], s_s[0], s_s+1);
+    printf("Difference: %.3f%ci%s\n", difference[R], d_s[0], d_s+1);
+    printf("Product: %.3f%ci%s\n", product[R], p_s[0], p_s+1);
+    printf("Quotient: %.3f%ci%s\n", quotient[R], q_s[0], q_s+1);
+
+    return 0;
+}
---
+++ 1. Wprowadzanie i wyświetlanie danych/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    float kek = -1.1;
+
+    printf("f: %+0.0f", kek);
+    return 0;
+}
