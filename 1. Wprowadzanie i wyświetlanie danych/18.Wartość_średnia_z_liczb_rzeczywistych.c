a8eda4e1ec8bfa43f1afc269ecf689ed66e12608
ScheiBig <scheissig@gmail.com>
2021-10-18 13:03:47 +0200

    add excercise 1.18

---
+++ 1. Wprowadzanie i wyświetlanie danych/18.Wartość_średnia_z_liczb_rzeczywistych.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    float n0, n1, n2, n3, n4;
+    printf("Input 5 numbers: \n");
+    scanf("%f %f %f %f %f",
+        &n0, &n1, &n2, &n3, &n4);
+    printf("Yours numbers average: %.4f\n1 ", 
+        (n0 + n1 + n2 + n3 + n4)
+         / 5);
+
+    return 0;
+}
