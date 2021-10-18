d8e98dfb838ab090c1cc3da805a410ff604ff7e6
ScheiBig <scheissig@gmail.com>
2021-10-18 22:25:07 +0200

    add excercise 1.29

---
+++ 1. Wprowadzanie i wyświetlanie danych/29.Wartość_maksymalna_i_minimalna.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int nums[2];
+    printf("Input your numbers: ");
+    scanf("%d %d", nums, nums+1);
+
+    int comp = nums[0] - nums[1];
+    int sign = ((unsigned)comp >> (sizeof(int)*8-1)) & 0x1;
+
+    printf("Maximum: %d\nMinimum: %d\n",
+        nums[sign], nums[1-sign]);
+
+    return 0;
+}
