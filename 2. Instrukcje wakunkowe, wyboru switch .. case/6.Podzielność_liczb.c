ae4d61cdfda8c9005539d68da3b2f564a44bfb85
unknown <209337@carbon.cti.p.lodz.pl>
2021-10-23 09:12:16 +0200

    add excercise 2.6

---
+++ 2. Instrukcje wakunkowe, wyboru switch .. case/6.Podzielność_liczb.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, success;
+    printf("Input first number: ");
+    success = scanf("%d", &num1);
+    if (success - 1)
+    {
+        printf("Incorrect input\n");
+        return 1;
+    }
+    printf("Input second number: ");
+    success = scanf("%d", &num2);
+    if (success - 1)
+    {
+        printf("Incorrect input\n");
+        return 1;
+    }
+    else if (num2 == 0)
+    {
+        printf("Operation not permitted\n");
+        return 1;
+    }
+
+    if (num1 % num2)
+    {
+        printf("%d is not divisible by %d\n", num1, num2);
+    }
+    else
+    {
+        printf("%d is divisible by %d\n", num1, num2);
+    }
+    
+    
+    
+    return 0;
+}
