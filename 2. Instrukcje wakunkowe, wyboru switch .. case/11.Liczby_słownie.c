d912a2c12727d7a6095d18a60e9a4e085450f63d
ScheiBig <scheissig@gmail.com>
2021-10-25 18:00:34 +0200

    add excercise 2.11

---
+++ 2. Instrukcje wakunkowe, wyboru switch .. case/11.Liczby_słownie.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+char dictionary [11][10] = {"zero", "jeden", "dwa", "trzy", "cztery",
+    "piec", "szesc", "siedem", "osiem", "dziewiec", "dziesiec"};
+
+int main()
+{
+    int num;
+    printf("Input your number: ");
+    if (scanf("%d", &num) != 1)
+    {
+        printf("Incorrect input\n");
+        return 1;
+    } 
+    else if (num < 0 || num > 10)
+    {
+        printf("Incorrect input data");
+        return 2;
+    }
+    printf("%s\n", dictionary[num]);
+    return 0;
+}
