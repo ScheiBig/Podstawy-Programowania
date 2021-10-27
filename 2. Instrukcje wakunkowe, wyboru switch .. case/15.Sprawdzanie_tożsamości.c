e5ea077048bf022c077aed657ff441415597ee35
ScheiBig <scheissig@gmail.com>
2021-10-27 23:30:59 +0200

    add excercise 2.15

---
+++ 2. Instrukcje wakunkowe, wyboru switch .. case/15.Sprawdzanie_tożsamości.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#define LEFT(A, B)  ((A + B) * (A - B))
+#define RIGHT(A, B) ((A * A) + (B * B))
+
+int main()
+{
+    float a, b;
+
+    printf("Input value a: ");
+    if (scanf("%f", &a) != 1)
+    {
+        printf("Incorrect input\n");
+        return 1;
+    }
+
+    printf("Input value b: ");
+    if (scanf("%f", &b) != 1)
+    {
+        printf("Incorrect input\n");
+        return 1;
+    }
+
+    if (RIGHT(a, b) == LEFT(a, b))
+    {
+        printf("tozsamosc jest prawdziwa\n");
+    }
+    else 
+    {
+        printf("tozsamosc nie jest prawdziwa\n");
+    }
+
+    return 0;
+}
