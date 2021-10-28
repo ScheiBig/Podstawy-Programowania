b7ce7b6bff082df890d7d1dddaf1fa8fb1342810
ScheiBig <scheissig@gmail.com>
2021-10-28 21:26:51 +0200

    add excercise 2.19

---
+++ 2. Instrukcje wakunkowe, wyboru switch .. case/19.Zmiana_na_wielkie_litery.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    char let[6];
+    printf("Input 5 letters: ");
+    scanf(" %c%c%c%c%c", let, let + 1, let + 2, let + 3, let + 4);
+
+    int diff = (int)('a' - 'A');
+
+    if (let[0] >= 'a' && let[0] <= 'z') let[0] -= diff;
+    if (let[1] >= 'a' && let[1] <= 'z') let[1] -= diff;
+    if (let[2] >= 'a' && let[2] <= 'z') let[2] -= diff;
+    if (let[3] >= 'a' && let[3] <= 'z') let[3] -= diff;
+    if (let[4] >= 'a' && let[4] <= 'z') let[4] -= diff;
+
+    let[5] = (char)0;
+
+    printf("%s\n", let);
+
+    return 0;
+}
