ad0677e3fff5751bdd30eb1cf10a03a2687d9ab7
ScheiBig <scheissig@gmail.com>
2021-10-31 23:35:40 +0100

    add excercise 2.27

---
+++ 2. Instrukcje wakunkowe, wyboru switch .. case/27.Parzyste_i_nieparzyste.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, success, comp;
+    printf("Input your number: ");
+    success = scanf("%d", &num);
+
+    comp = 10 * (success == 1) + num % 2;
+
+    switch (comp)
+    {
+    case 10:
+        printf("%d is even\n", num);
+        break;
+    case 9:
+    case 11:
+        printf("%d is odd\n", num);
+        break;
+    default:
+        printf("Incorrect input\n");
+        return 1;
+    }
+
+    return 0;
+}
