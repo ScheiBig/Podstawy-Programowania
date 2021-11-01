7c8d91d6681cf70063781e8260afee5d67ceffb2
ScheiBig <scheissig@gmail.com>
2021-11-01 22:31:47 +0100

    add excercise 2.30

---
+++ 2. Instrukcje wakunkowe, wyboru switch .. case/30.Promocja.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int grade;
+    printf("Enter your grade: ");
+    scanf("%d", &grade);
+    switch (grade)
+    {
+    case 1:
+        printf("brak promocji do nastepnej klasy\n");
+        break;
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        printf("promocja do nastepnej klasy\n");
+        break;
+    case 6:
+        printf("promocja z ocena celujaca\n");
+    default:
+        printf("Wprowadzona ocena jest niepoprawna.\n");
+    }
+    return 0;
+}
