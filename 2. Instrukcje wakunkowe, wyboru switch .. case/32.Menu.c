ebcc7cc9e04400755dd2e5054ea3ddfef79988c8
ScheiBig <scheissig@gmail.com>
2021-11-01 23:11:58 +0100

    add excercise 2.32

---
+++ 2. Instrukcje wakunkowe, wyboru switch .. case/32.Menu.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+    char op;
+
+    printf("Input numbers: ");
+    switch (scanf("%d %d", &num1, &num2))
+    {
+    case 2:
+        break;
+    default:
+        printf("Incorrect input\n");
+        return 1;
+    }
+    printf("Input operation symbol: ");
+    switch (scanf(" %c", &op))
+    {
+    case 1:
+        break;
+    default:
+        printf("Incorrect input\n");
+        return 1;
+    }
+    switch (op)
+    {
+    case '+':
+        printf("%d\n", num1 + num2);
+        break;
+    case '-':
+        printf("%d\n", num1 - num2);
+        break;
+    case '*':
+        printf("%d\n", num1 * num2);
+        break;
+    case '/':
+        switch (num2)
+        {
+        case 0:
+            printf("Illegal operation\n");
+            return 2;
+        default:
+            printf("%.2f\n", (float)num1 / num2);
+            break;
+        }
+        break;
+    default:
+        printf("Incorrect input\n");
+        return 1;
+    }
+        
+    return 0;
+}
