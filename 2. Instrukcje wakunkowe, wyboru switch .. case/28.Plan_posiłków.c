c197a9a4a93d7c0da0a66ac5846d69b8b6285b0c
ScheiBig <scheissig@gmail.com>
2021-11-01 19:12:19 +0100

    add excercise 2.28

---
+++ 2. Instrukcje wakunkowe, wyboru switch .. case/28.Plan_posiłków.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+typedef unsigned char uchar;
+
+int main()
+{
+    uchar choice, plan_content[3][32] = {
+        "Breakfast, Lunch and Dinner",
+        "Lunch and Dinner",
+        "Dinner"
+    };
+    int success, plan;
+    printf("Enter your choice: ");
+    success = scanf(" %c", &choice);
+    choice *= success == 1;
+    choice *= choice >= 'A' && choice <= 'C';
+
+    switch (choice)
+    {
+    case (uchar)0:
+        printf("Incorrect input\n");
+        return 1;
+    default:
+        plan = choice - 'A';
+    }
+
+    printf("You chose %s as your meal plan.\n", plan_content[plan]);
+    return 0;
+}
