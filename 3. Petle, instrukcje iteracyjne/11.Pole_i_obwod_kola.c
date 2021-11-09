8c5757df601fb4cca655d29868e5cd80a013c75c
Marcin Jeznach <Scheissig@gmail.com>
2021-11-09 22:36:33 +0100

    add exercise 3.11

---
+++ 3. Petle, instrukcje iteracyjne/11.Pole_i_obwod_kola.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+#define PI (atan(1) * 4)
+
+int main()
+{
+    float radius = 0;
+    char buffer[32];
+    while (1)
+    {
+        printf("Input circle radius: ");
+        fgets(buffer, sizeof buffer, stdin);
+        radius *= sscanf(buffer, "%f", &radius) == 1;
+        if (radius > 1)
+        { break; }
+        else
+        { printf("Incorrect input\n"); }
+    }
+    printf("Circumference: %.3f\n", 2 * radius * PI);
+    printf("Area: %.3f\n", radius * radius * PI);
+
+    return 0;
+}
