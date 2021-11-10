01e9ea5028cddf8f6e1a508559f586a1571d8230
Marcin Jeznach <Scheissig@gmail.com>
2021-11-10 20:14:15 +0100

    add exercise 3.12

---
+++ 3. Petle, instrukcje iteracyjne/12.Silnia.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef unsigned long long int ulonger;
+
+int main()
+{
+    int number;
+    printf("Enter your number: ");
+    if (scanf("%d", &number) != 1 || number < 0 || number > 21)
+    {
+        printf("Incorrect input");
+        return 1;
+    }
+    ulonger factorial = 1;
+    for (int i = 1; i < number; factorial *= ++i);
+    printf("%llu", factorial);
+
+    return 0;
+}
