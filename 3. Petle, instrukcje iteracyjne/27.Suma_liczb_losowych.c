f16f0011b13424c516f2d1f8f313616ac572038c
Marcin Jeznach <Scheissig@gmail.com>
2021-11-16 22:26:16 +0100

    add exercise 3.27

---
+++ 3. Petle, instrukcje iteracyjne/27.Suma_liczb_losowych.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    srand(time(NULL));
+
+    long sum = 0;
+
+    for (int i = 0; i < 100; ++i)
+    { sum += rand() % (21 - 10) + 10; }
+
+    printf("%ld", sum);
+
+    return 0;
+}
