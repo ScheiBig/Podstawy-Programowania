698de5010059fd396ea4fe4421382c2612355e8b
ScheiBig <scheissig@gmail.com>
2022-01-04 22:08:32 +0100

    add exercise 7.4

---
+++ 7.Funkcje I, II/4.Znak_liczby.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "my_utils.h"
+#include <math.h>
+
+int sign(int value);
+
+enum sign
+{
+    negative = -1, zero = 0, positive = 1
+};
+
+int main()
+{
+    int val;
+    print_ln("Enter a number:");
+
+    //NOLINTNEXTLINE(clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling) -> Dante doesn't provide scanf_s
+    switch (scanf("%d", &val))
+    {
+    case 1: {
+        break;
+    }
+    default:
+        _e_exit(1, "Incorrect input")
+    }
+    
+    switch (sign(val))
+    {
+    case negative:
+        print_ln("negative");
+        break;
+    case zero:
+        print_ln("zero");
+        break;
+    case positive:
+        print_ln("positive");
+        break;
+    }
+
+    return 0;
+}
+
+int sign(int value)
+{
+    return negative * (value < 0) + zero * (value == 0) + positive * (value > 0);
+}
