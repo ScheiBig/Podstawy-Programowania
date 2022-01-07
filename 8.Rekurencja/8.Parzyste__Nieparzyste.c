df7e64b54f835eef24fc535caab9577463103f84
ScheiBig <scheissig@gmail.com>
2022-01-07 20:51:38 +0100

    add exercise 8.8

--- 8.Rekurencja/8.Parzyste__Nieparzyste.c
+++ 8.Rekurencja/8.Parzyste__Nieparzyste.c
@@ -2,30 +2,42 @@
 #include <stdlib.h>
 #include "my_utils.h"
 
-int GCD(int a, int b);
+int even_odd(int s, int n);
 
 int main()
 {
     int a, b;
-    print_ln("Enter a first number:");
+    print_ln("Enter a number:");
     // NOLINTNEXTLINE(clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling) -> Dante doesn't provide *_s
     if (scanf("%d", &a) != 1)
     {
         _e_exit(1, "Incorrect input")
     }
-    print_ln("Enter a second number:");
+    print_ln("Enter a index:");
     // NOLINTNEXTLINE(clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling) -> Dante doesn't provide *_s
     if (scanf("%d", &b) != 1)
     {
         _e_exit(1, "Incorrect input")
     }
+    else if (b < 0)
+    {
+        _e_exit(2, "Incorrect input data")
+    }
 
-    printf_ln("%d", GCD(a, b));
+    printf_ln("%d", even_odd(a, b));
     return 0;
 }
 
-int GCD(int a, int b)
+int even_odd(int s, int n)
 {
-    if (b == 0) return abs(a);
-    else return GCD(b, a % b);
+    if (n < 0) return -1;
+    else if (n == 0) return s;
+    else
+    {
+        int ret = even_odd(s, n - 1);
+        if (ret % 2 == 0)
+            return ret / 2;
+        else
+            return 3 * ret + 1;
+    }
 }
