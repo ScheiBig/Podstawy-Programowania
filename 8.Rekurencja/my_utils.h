ebe95a62f5222ce4331b845b91305c8a7245acd8
ScheiBig <scheissig@gmail.com>
2022-01-09 00:56:56 +0100

    add exercise 8.11

--- 8.Rekurencja/my_utils.h
+++ 8.Rekurencja/my_utils.h
@@ -4,8 +4,8 @@
 // (C) Marcin Jeznach (TM), plz no steal
 
 #include <stdio.h>
-#define eprintf_ln(msg) fprintf(stderr, msg "\n" )
-#define _e_exit(eno, msg) { eprintf_ln(msg); return eno; }
+#define eprint_ln(msg) fprintf(stderr, msg "\n" )
+#define _e_exit(eno, msg) { eprint_ln(msg); return eno; }
 #define printf_ln(format, ...) printf(format "\n", __VA_ARGS__)
 #define print_ln(format) printf(format "\n")
 #define print_n() printf("\n")
@@ -20,12 +20,14 @@
 #define is_letter(chr) ('a' <= chr && chr <= 'z' || 'A' <= chr && chr <= 'Z')
 #define is_uppercase(chr) ('A' <= chr && chr <= 'Z')
 #define is_lowercase(chr) ('a' <= chr && chr <= 'z')
-#define is_number(chr) ('0' <= chr && chr <= '9')
+#define is_digit(chr) ('0' <= chr && chr <= '9')
+#define ctoi(chr) (chr - '0')
 
 #define cond_assign_l(to, arg) (to = to < arg ? arg : to)
 #define cond_assign_le(to, arg) (to = to <= arg ? arg : to)
 #define cond_assign_g(to, arg) (to = to > arg ? arg : to)
 #define cond_assign_ge(to, arg) (to = to >= arg ? arg : to)
+#define double_almost_eq(arg, to, eps) ((to - eps) < arg && arg < (to + eps))
 
 #define REGISTER_SWAP(type) \
     void swap_int(type* a1, type* a2)\
