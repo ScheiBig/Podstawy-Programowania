3f5b0f5f9bc361ef437c99a7c6633c121b268de8
ScheiBig <scheissig@gmail.com>
2022-01-09 12:11:29 +0100

    add exercise 9.1

---
+++ 9.Sortowanie/vector_utils.h
@@ -0,0 +1,14 @@
+#ifndef PODSTAWY_PROGRAMOWANIA_1_VECTOR_UTILS_H
+#define PODSTAWY_PROGRAMOWANIA_1_VECTOR_UTILS_H
+
+int read_vector(int vec[], int size, int stop_value);
+void display_vector(const int vec[], int size);
+
+#define vuDATA_invalid -2
+#define vuVEC_invalid -1
+
+#ifndef vuEOV
+#define vuEOV 0
+#endif
+
+#endif
