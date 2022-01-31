dcfac134edcb58b7a04a2a6ecc8e0671e7ca8f24
ScheiBig <scheissig@gmail.com>
2022-02-01 00:38:40 +0100

    add exercise 9.3

--- 9.Sortowanie/sort.c
+++ 9.Sortowanie/sort.c
@@ -1,6 +1,9 @@
 #include "sort.h"
 #include "vector_utils.h"
+#include "my_utils.h"
 #include <stdio.h>
+
+REGISTER_SWAP(int)
 
 int bubble_sort(int tab[], int size, enum direction dir)
 {
@@ -26,7 +29,7 @@ int bubble_sort_asc(int tab[], int size)
         display_vector(tab, size);
         return 0;
     }
-    int temp, mov;
+    int mov;
     for (int i = size; i > 0; --i)
     {
         mov = 0;
@@ -34,9 +37,7 @@ int bubble_sort_asc(int tab[], int size)
         {
             if (tab[j] > tab[j + 1])
             {
-                temp = tab[j];
-                tab[j] = tab[j + 1];
-                tab[j + 1] = temp;
+                swap_int(tab + j, tab + i);
                 ++mov;
             }
         }
@@ -54,7 +55,7 @@ int bubble_sort_desc(int tab[], int size)
         display_vector(tab, size);
         return 0;
     }
-    int temp, mov;
+    int mov;
     for (int i = size; i > 0; --i)
     {
         mov = 0;
@@ -62,14 +63,78 @@ int bubble_sort_desc(int tab[], int size)
         {
             if (tab[j] < tab[j + 1])
             {
-                temp = tab[j];
-                tab[j] = tab[j + 1];
-                tab[j + 1] = temp;
+                swap_int(tab + j, tab + i);
                 ++mov;
             }
         }
         display_vector(tab, size);
         if (mov == 0) break;
+    }
+    return 0;
+}
+
+int selection_sort(int tab[], int size, enum direction dir)
+{
+    if (size < 1) return 1;
+    if (size == 1)
+    {
+        display_vector(tab, size);
+        return 0;
+    }
+    switch ((int)dir) // Great idea to use two different static checkers - GCC want exhaustive switch, clang thinks default is unreachable
+    {
+    case ASCENDING: return selection_sort_asc(tab, size);
+    case DESCENDING: return selection_sort_desc(tab, size);
+    }
+    return 1;
+}
+
+int selection_sort_asc(int tab[], int size)
+{
+    if (size < 1) return 1;
+    if (size == 1)
+    {
+        display_vector(tab, size);
+        return 0;
+    }
+    int min;
+    for (int i = 0; i < size; ++i)
+    {
+        min = i;
+        for (int j = i + 1; j < size; ++j)
+        {
+            if (tab[j] < tab[min])
+            {
+                min = j;
+            }
+        }
+        swap_int(tab + i, tab + min);
+        display_vector(tab, size);
+    }
+    return 0;
+}
+
+int selection_sort_desc(int tab[], int size)
+{
+    if (size < 1) return 1;
+    if (size == 1)
+    {
+        display_vector(tab, size);
+        return 0;
+    }
+    int max;
+    for (int i = 0; i < size; ++i)
+    {
+        max = i;
+        for (int j = i + 1; j < size; ++j)
+        {
+            if (tab[j] > tab[max])
+            {
+                max = j;
+            }
+        }
+        swap_int(tab + i, tab + max);
+        display_vector(tab, size);
     }
     return 0;
 }
