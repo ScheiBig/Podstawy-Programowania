35763de22a0fa267fa567a126c38bd3d0a0b3060
ScheiBig <scheissig@gmail.com>
2022-02-01 01:10:01 +0100

    add exercise 9.4

--- 9.Sortowanie/sort.c
+++ 9.Sortowanie/sort.c
@@ -138,3 +138,65 @@ int selection_sort_desc(int tab[], int size)
     }
     return 0;
 }
+
+int insertion_sort(int tab[], int size, enum direction dir)
+{
+    if (size < 1) return 1;
+    if (size == 1)
+    {
+        display_vector(tab, size);
+        return 0;
+    }
+    switch ((int)dir) // Great idea to use two different static checkers - GCC want exhaustive switch, clang thinks default is unreachable
+    {
+    case ASCENDING: return insertion_sort_asc(tab, size);
+    case DESCENDING: return insertion_sort_desc(tab, size);
+    }
+    return 1;
+}
+
+int insertion_sort_asc(int tab[], int size)
+{
+    if (size < 1) return 1;
+    if (size == 1)
+    {
+        display_vector(tab, size);
+        return 0;
+    }
+    int curr, j;
+    for (int i = 1; i < size; ++i)
+    {
+        curr = tab[i];
+        for (j = i - 1; j >= 0 && tab[j] > curr; --j)
+        {
+            tab[j + 1] = tab[j];
+        }
+        tab[j + 1] = curr;
+        display_vector(tab, size);
+    }
+    display_vector(tab, size);
+    return 0;
+}
+
+int insertion_sort_desc(int tab[], int size)
+{
+    if (size < 1) return 1;
+    if (size == 1)
+    {
+        display_vector(tab, size);
+        return 0;
+    }
+    int curr, j;
+    for (int i = 1; i < size; ++i)
+    {
+        curr = tab[i];
+        for (j = i - 1; j >= 0 && tab[j] < curr; --j)
+        {
+            tab[j + 1] = tab[j];
+        }
+        tab[j + 1] = curr;
+        display_vector(tab, size);
+    }
+    display_vector(tab, size);
+    return 0;
+}
--- 9.Sortowanie/sort.h
+++ 9.Sortowanie/sort.h
@@ -12,3 +12,7 @@ int bubble_sort(int tab[], int size, enum direction dir);
 int selection_sort_asc(int tab[], int size);
 int selection_sort_desc(int tab[], int size);
 int selection_sort(int tab[], int size, enum direction dir);
+
+int insertion_sort_asc(int tab[], int size);
+int insertion_sort_desc(int tab[], int size);
+int insertion_sort(int tab[], int size, enum direction dir);
