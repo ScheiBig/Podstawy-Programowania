e15a5b55c536998a5ffcade5670f00d84a33ffbe
ScheiBig <scheissig@gmail.com>
2022-02-01 22:30:24 +0100

    add exercise 9.7

---
+++ 9.Sortowanie/bubble_sort.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "types_utils.h"
+
+int bubble_sort_asc_rec(int tab[], int size);
+int bubble_sort_desc_rec(int tab[], int size);
+int bubble_sort_rec(int tab[], int size, enum direction dir);
