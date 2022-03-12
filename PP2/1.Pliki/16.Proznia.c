318944d0bcdeac36a865bb427f24a6896540b603
ScheiBig <scheissig@gmail.com>
2022-03-12 17:45:49 +0100

    add exercise 2.1.16

---
+++ PP2/1.Pliki/16.Proznia.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+#include <time.h>
+
+// #define __discard_stdin__
+#include "my_utils.h"
+#include "my_exit.h"
+#include "my_file.h"
+
+#include <errno.h>
+
+int main()
+{
+    register_exit_handler(close_file_pointers);
+    char buffer[31];
+
+    print_ln("Enter file path:");
+    //NOLINTNEXTLINE(clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling) -> Dante doesn't provide *_s
+    if (scanf("%30s", buffer) != 1)
+    {
+        _mh_exit(eFILE_noaccess, eFILE_noaccess_msg)
+    }
+
+    if (register_file_pointer(fopen(buffer, "r")))
+    {
+        print_ln("File already exists!");
+    }
+    else
+    {
+        if (register_file_pointer(fopen(buffer, "w")))
+        {
+            print_ln("File created");
+        }
+        else
+        {
+            _mh_exit(eFILE_cantcreate, eFILE_cantcreate_msg)
+        }
+    }
+
+    _h_exit()
+}
