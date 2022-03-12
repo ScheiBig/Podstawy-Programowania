d27d3bfbbc68c93a684416aabffd19a6aee1e67c
ScheiBig <scheissig@gmail.com>
2022-03-12 14:00:57 +0100

    add exercise 2.1.11

---
+++ PP2/1.Pliki/11.Nie_tekstowy.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+#include <time.h>
+
+#define __discard_stdin__
+#include "my_utils.h"
+#include "my_exit.h"
+#include "my_file.h"
+
+#include <errno.h>
+
+int main()
+{
+    register_exit_handler(close_file_pointers);
+    srand(time(NULL));
+    char buffer[31];
+
+    print_ln("Enter file path:");
+    //NOLINTNEXTLINE(clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling) -> Dante doesn't provide *_s
+    if (scanf("%30s", buffer) != 1)
+    {
+        _mh_exit(eFILE_cantcreate, eFILE_cantcreate_msg)
+    }
+    FILE* out_file = register_file_pointer(fopen(buffer, "wb"));
+    if (out_file == NULL)
+    {
+        _mh_exit(eFILE_cantcreate, eFILE_cantcreate_msg)
+    }
+    discard_stdin();
+
+    float nums[100];
+    for (float* s = nums; s < nums + 100; ++s)
+    {
+        *s = clip_range(rand(), -10000, 10000) / 100.0f;
+    }
+    fwrite(nums, sizeof(*nums), 100, out_file);
+    
+    print_ln("File saved");
+    _h_exit()
+}
