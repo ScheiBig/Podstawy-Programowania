2e42073ffb7b9dbc89e79d88b97ffff86fb6d09c
ScheiBig <scheissig@gmail.com>
2022-03-12 16:17:34 +0100

    add exercise 2.1.13

---
+++ PP2/1.Pliki/13.Pokaz_kotku,_co_masz_w_srodku.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+#include <time.h>
+
+#define __discard_stdin__
+#include "my_utils.h"
+#include "my_exit.h"
+#include "my_file.h"
+
+#include <errno.h>
+
+int main()
+{
+    register_exit_handler(close_file_pointers);
+    char buffer[31];
+
+    print_ln("Enter input file path:");
+    //NOLINTNEXTLINE(clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling) -> Dante doesn't provide *_s
+    if (scanf("%30s", buffer) != 1)
+    {
+        _mh_exit(eFILE_noaccess, eFILE_noaccess_msg)
+    }
+    FILE* in_file = register_file_pointer(fopen(buffer, "r"));
+    if (in_file == NULL)
+    {
+        _mh_exit(eFILE_noaccess, eFILE_noaccess_msg)
+    }
+    discard_stdin();
+
+    unsigned char buff_char;
+
+    while (fread(&buff_char, sizeof(buff_char), 1, in_file))
+    {
+        printf("%u ", buff_char);
+    }
+
+    print_ln("File saved");
+    _h_exit()
+}
