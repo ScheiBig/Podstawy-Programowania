8534ce4b74babe185e8da2548626c63dfe6ca229
ScheiBig <scheissig@gmail.com>
2022-03-31 14:52:54 +0200

    add exercise 2.1.21[R]

---
+++ PP2/1.Pliki/21.Ukryta_wiadomosc.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+#include <time.h>
+
+#define __discard_stdin__
+#include "my_utils.h"
+#include "my_exit.h"
+#include "my_file.h"
+
+int main()
+{
+    register_exit_handler(close_file_pointers);
+    char buffer[31];
+
+    print_ln("Enter input file path:");
+    //NOLINTNEXTLINE(clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling) -> Dante doesn't provide *_s
+    if (scanf("%30s", buffer) != 1)
+    {
+        _mh_exit(eFILE_noaccess, eFILE_noaccess_msg)
+    }
+
+    FILE* in_file = register_file_pointer(fopen(buffer, "r"));
+    if (in_file == NULL)
+    {
+        _mh_exit(eFILE_noaccess, eFILE_noaccess_msg)
+    }
+    discard_stdin();
+
+    char cur_char;
+    while (true)
+    {
+        if ((cur_char = fgetc(in_file)) == '\n')
+        {
+            break;
+        }
+        else if (is_digit(cur_char) || cur_char == ' ')
+        {
+            continue;
+        }
+        else
+        {
+            _eh_exit(eFILE_corrupted, eFILE_corrupted_msg)
+        }
+    }
+    fseek(in_file, 0L, SEEK_END);
+    long max_pos = ftell(in_file);
+    fseek(in_file, 0L, SEEK_SET);
+
+    long next_pos;
+    long cur_pos;
+    //NOLINTNEXTLINE(clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling) -> Dante doesn't provide *_s
+    while (fscanf(in_file, " %ld", &next_pos) == 1)
+    {
+        if (next_pos >= max_pos)
+        {
+            _eh_exit(eFILE_corrupted, eFILE_corrupted_msg)
+        }
+    }
+    fseek(in_file, 0L, SEEK_SET);
+    //NOLINTNEXTLINE(clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling) -> Dante doesn't provide *_s
+    while (fscanf(in_file, " %ld", &next_pos) == 1)
+    {
+        cur_pos = ftell(in_file);
+        fseek(in_file, next_pos, SEEK_SET);
+        fputc(fgetc(in_file), stdout);
+        fseek(in_file, cur_pos, SEEK_SET);
+    }
+    _h_exit()
+}
